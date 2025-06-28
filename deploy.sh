9a6e74bee536636ad491a60a0725be5f2fd24d85
MentaalAchtergesteld <mentaalachtergesteld@mentaalachtergesteld.nl>
2025-06-28 14:19:09 +0200

    improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,13 @@
 set -e
 
 PI_USER="mentaal"
-PI_HOST="raspberrypi"
+PI_HOST="raspberrypi.local"
 REMOTE_DIR="/home/mentaal/website"
 BINARY_NAME="personal-website"
 BINARY_LOCATION="armv7-unknown-linux-musleabihf"
+
+echo "Compiling..."
+cross build --target=armv7-unknown-linux-musleabihf --release
 
 echo "Stopping service..."
 ssh $PI_USER@$PI_HOST "sudo systemctl stop personal-website"
@@ -17,7 +20,7 @@ scp target/$BINARY_LOCATION/release/$BINARY_NAME \
 scp .env $PI_USER@$PI_HOST:$REMOTE_DIR
 
 echo "Synchronizing static files..."
-rsync -av --delete static/ $PI_USER@$PI_HOST:$REMOTE_DIR/static
+rsync -av --delete --checksum static/ $PI_USER@$PI_HOST:$REMOTE_DIR/static
 
 echo "Restarting service..."
 ssh $PI_USER@$PI_HOST "sudo systemctl restart personal-website"
