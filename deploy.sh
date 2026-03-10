010866eb4bb1edb1c4150dcdfdc0d2b729abd427
MentaalAchtergesteld <mentaalachtergesteld@mentaalachtergesteld.nl>
2026-03-10 17:39:20 +0100

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ echo "[1/4] Building container..."
 docker buildx build --platform linux/arm64 -t pi-site:latest --load .
 
 echo "[2/4] Uploading to Raspberry PI"
-docker save pi-site:latest | ssh -C $PI_USER@$PI_HOST "docker load"
+docker save pi-site:latest | ssh -C $PI_HOST "docker load"
 
 echo "[3/4] Syncinv .env."
-scp .env $PI_USER@$PI_HOST:$DIR/.env
+scp .env $PI_HOST:$DIR/.env
 
 echo "[4/4] Restarting container..."
 ssh $PI_HOST "cd $DIR && docker compose up -d --force-recreate app"
